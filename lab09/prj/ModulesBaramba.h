74b2b30640253a42868eaa264b375ecab2c51ca4
croVVhite <c.crovvhite.w@gmail.com>
2022-03-24 22:57:06 +0200

    added header file ModulesBaramba.h

---
+++ lab09/prj/ModulesBaramba.h
@@ -0,0 +1,12 @@
+#ifndef MODULESBARAMBA_H_INCLUDED
+#define MODULESBARAMBA_H_INCLUDED
+
+#include <iostream>
+#include <cmath>
+
+double s_calculation(float x, float z);
+string BeaufortScale(int BeaufortScore);
+string helmetSize(int headSize);
+int binOnesZeros(int number);
+
+#endif // MODULESBARAMBA_H_INCLUDED
