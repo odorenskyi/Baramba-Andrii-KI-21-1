c140156fda75e307ccc7a58f6fc8fde6422a6651
croVVhite <c.crovvhite.w@gmail.com>
2022-04-27 15:20:44 +0300

    recursion is replaced by a loop and notation was simplified

--- lab11/prj/ModulesBaramba/main.cpp
+++ lab11/prj/ModulesBaramba/main.cpp
@@ -2,22 +2,14 @@
 
 using namespace std;
 
-void writeToRegistData(fstream &file, regEnrollment *regEn) {
-
-    if (regEn->ptr != nullptr) {
-        writeToRegistData(file, regEn->ptr);
+void writeToRegistData(fstream &file, regEnrollment *regEn)
+{
+    regEnrollment *buffer = regEn;
+    file.write((char*)&*buffer, sizeof(regEnrollment));
+    while (buffer->ptr != nullptr) {
+        buffer = buffer->ptr;
+        file.write((char*)&*buffer, sizeof(regEnrollment));
     }
-
-    file.write(&regEn->firstName[0], PROPER_NAME_SIZE);
-    file.write(&regEn->lastName[0], PROPER_NAME_SIZE);
-    file.write(&regEn->patronymic[0], PROPER_NAME_SIZE);
-    file.write(&regEn->carBrand[0], PROPER_NAME_SIZE);
-    file.write(&regEn->gradYear[0], YEAR_STIRNG_SIZE);
-    file.write(&regEn->dateDay[0], DATE_STRING_SIZE);
-    file.write(&regEn->dateMonth[0], DATE_STRING_SIZE);
-    file.write(&regEn->dateYear[0], YEAR_STIRNG_SIZE);
-    file.write(&regEn->govNumber[0], GOV_NUMBER_SIZE);
-    file.write(&regEn->additions[0], LARGE_TXT_SIZE);
 }
 
 void writeToFile(fstream &file, regEnrollment *regEn) {
